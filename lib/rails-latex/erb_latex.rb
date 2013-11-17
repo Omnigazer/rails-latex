0ccb615b3fe8e15a1a68b6551202830fa0e24dd1
Omnigazer <omnigazer@gmail.com>
2013-11-17 20:53:33 +0300

    Update erb_latex.rb

--- lib/rails-latex/erb_latex.rb
+++ lib/rails-latex/erb_latex.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 require 'fileutils'
 require 'rails-latex/latex_to_pdf'
+require 'rails-latex/latex_to_rtf'
 require 'action_view'
 
 module ActionView               # :nodoc: all
@@ -12,8 +13,14 @@ module ActionView               # :nodoc: all
 
       def compile(template)
         erb = "<% __in_erb_template=true %>#{template.source}"
+        _format = template.inspect.to_s.split(".erbtex")[0].split(".")[-1]
         out=self.class.erb_implementation.new(erb, :trim=>(self.class.erb_trim_mode == "-")).src
-        out + ";LatexToPdf.generate_pdf(@output_buffer.to_s,@latex_config||{},@latex_parse_twice)"
+        case _format
+        when "rtf" then
+          out + ";LatexToRtf.generate_rtf(@output_buffer.to_s,@latex_config||{}"
+        else
+          out + ";LatexToPdf.generate_pdf(@output_buffer.to_s,@latex_config||{},@latex_parse_twice)"  
+        end
       end
     end
   end
